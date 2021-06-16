f8683d0829a71932a8eb93da078e462f144d6cf9
Igor Polousov <igorpolousov@gmail.com>
2021-06-16 13:43:11 +0300

    Initial Commit

---
+++ Guess the Flag/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by Igor Polousov on 16.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
